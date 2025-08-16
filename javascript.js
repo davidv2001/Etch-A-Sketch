6a3cb107f5ef0416c5af430a7c1bb60d293f0381
David Valencia <davidv2001learning@gmail.com>
2025-08-16 07:25:49 -0400

    hover-effect

--- javascript.js
+++ javascript.js
@@ -9,3 +9,19 @@ for (let i = 0; i < 16 * 16; i++) {
   grid.appendChild(cell);
 }
 container.appendChild(grid);
+
+const cells = document.querySelectorAll(".cell");
+// const cells = [...cellsNodelist];
+
+// console.log(cells);
+
+// cells.forEach((cell) =>
+//   cell.addEventListener(
+//     "mouseenter",
+//     () => (cell.style.backgroundColor = "grey")
+//   )
+// );
+
+cells.forEach((cell) =>
+  cell.addEventListener("mouseenter", () => cell.classList.add("hover"))
+);
